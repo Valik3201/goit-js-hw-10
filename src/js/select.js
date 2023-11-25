b31ee26e52ccf8d47f4e71f5b0ca4334321f9dd9
Valik <valik3201@gmail.com>
2023-11-26 00:38:38 +0100

    feat: added multi select

--- src/js/select.js
+++ src/js/select.js
@@ -1,12 +1,12 @@
 import SlimSelect from "slim-select";
 import "slim-select/dist/slimselect.css";
 
-import { fetchBreeds } from "./cat-api";
-
 export const populateBreedsSelect = (breeds) => {
   const select = new SlimSelect({
     select: ".breed-select",
-    placeholder: "Select a breed",
-    data: breeds.map((breed) => ({ text: breed.name, value: breed.id })),
+    data: [
+      { placeholder: true, text: "Search" },
+      ...breeds.map((breed) => ({ text: breed.name, value: breed.id })),
+    ],
   });
 };
