0e7eb6093e96835c77b5be49070e6b8daa68af2d
Valik <valik3201@gmail.com>
2023-11-26 20:09:08 +0100

    refactor: updated `index.js`

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,12 @@
-// index.js
 import { fetchBreeds } from "./cat-api";
 import { populateBreedsSelect } from "./select";
+import { showError } from "./feedbackHandling";
 
 fetchBreeds()
   .then((breeds) => {
     populateBreedsSelect(breeds);
   })
   .catch((error) => {
+    showError();
     console.error("Error:", error);
   });
