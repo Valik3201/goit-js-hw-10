4860f78a04feb341f801d46899847d1a15aa2606
Valik <valik3201@gmail.com>
2023-11-26 20:08:43 +0100

    feat: added cat breed info generation

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,14 +1,39 @@
 import axios from "axios";
+import { showLoader, hideLoader, showError } from "./feedbackHandling";
 
 axios.defaults.headers.common["x-api-key"] =
   "live_17W11S6QfjcvMt6gMvLcPsE6GzJxkEVugauVifkT0ILTFEZkRYDTpx3gGp29m6Jh";
 
 export const fetchBreeds = () => {
+  const breedSelect = document.querySelector(".breed-select");
+  showLoader();
   return axios
     .get("https://api.thecatapi.com/v1/breeds")
-    .then((response) => response.data)
+    .then((response) => {
+      hideLoader();
+      breedSelect.style.display = "flex";
+      return response.data;
+    })
     .catch((error) => {
+      hideLoader();
+      showError();
       console.error("Error fetching breeds:", error);
+      throw error;
+    });
+};
+
+export const fetchCatByBreed = (breedId) => {
+  showLoader();
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then((response) => {
+      hideLoader();
+      return response.data;
+    })
+    .catch((error) => {
+      hideLoader();
+      showError();
+      console.error(`Error fetching cat for breed ${breedId}:`, error);
       throw error;
     });
 };
