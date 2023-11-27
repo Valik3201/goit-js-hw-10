2b73a8949173442dad9f4706df8e1d91c2bf8a3c
Valik <valik3201@gmail.com>
2023-11-27 15:28:35 +0100

    refactor: сhanged functions to asynchronous

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -8,78 +8,78 @@ axios.defaults.headers.common["x-api-key"] =
   "live_17W11S6QfjcvMt6gMvLcPsE6GzJxkEVugauVifkT0ILTFEZkRYDTpx3gGp29m6Jh";
 
 /**
- * Fetches a list of cat breeds.
+ * Asynchronously fetches a list of cat breeds.
  * @function
  * @returns {Promise<Array>} A Promise that resolves to an array of cat breeds.
  * @throws {Error} If the request to fetch breeds fails.
  */
-export const fetchBreeds = () => {
+export const fetchBreeds = async () => {
   // Get the breed select element from the DOM
   const breedSelect = document.querySelector(".breed-select");
 
   // Show loader while fetching breeds
   showLoader();
 
-  /**
-   * Axios GET request to fetch cat breeds.
-   * @type {Promise<Array>}
-   */
-  return axios
-    .get("https://api.thecatapi.com/v1/breeds")
-    .then((response) => {
-      // Hide loader on successful response
-      hideLoader();
+  try {
+    /**
+     * Response object from the API containing cat breeds.
+     * @type {object}
+     */
+    const response = await axios.get("https://api.thecatapi.com/v1/breeds");
 
-      // Display the breed select element
-      breedSelect.style.display = "flex";
+    // Hide loader on successful response
+    hideLoader();
 
-      // Return the array of cat breeds
-      return response.data;
-    })
-    .catch((error) => {
-      // Hide loader on error and show error message
-      hideLoader();
-      showError(error);
+    // Display the breed select element
+    breedSelect.style.display = "flex";
 
-      // Log the error to the console
-      console.error("Error fetching breeds:", error);
+    // Return the array of cat breeds
+    return response.data;
+  } catch (error) {
+    // Hide loader on error and show error message
+    hideLoader();
+    showError(error);
 
-      // Propagate the error
-      throw error;
-    });
+    // Log the error to the console
+    console.error("Error fetching breeds:", error);
+
+    // Propagate the error
+    throw error;
+  }
 };
 
 /**
- * Fetches information about a specific cat breed.
+ * Asynchronously fetches information about a specific cat breed.
  * @function
  * @param {string} breedId - The ID of the cat breed.
  * @returns {Promise<Object>} A Promise that resolves to an object containing cat breed information.
  * @throws {Error} If the request to fetch the cat breed information fails.
  */
-export const fetchCatByBreed = (breedId) => {
+export const fetchCatByBreed = async (breedId) => {
   // Show loader while fetching cat breed information
   showLoader();
 
-  /**
-   * Axios GET request to fetch information about a specific cat breed.
-   * @type {Promise<Object>}
-   */
-  return axios
-    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-    .then((response) => {
-      // Hide loader on successful response and return the cat breed information
-      hideLoader();
-      return response.data;
-    })
-    .catch((error) => {
-      // Hide loader on error and show error message
-      hideLoader();
-      showError(error);
+  try {
+    /**
+     * Response object from the API containing cat breed information.
+     * @type {object}
+     */
+    const response = await axios.get(
+      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+    );
 
-      // Log the error to the console
-      console.error(`Error fetching cat for breed ${breedId}:`, error);
+    // Hide loader on successful response and return the cat breed information
+    hideLoader();
+    return response.data;
+  } catch (error) {
+    // Hide loader on error and show error message
+    hideLoader();
+    showError(error);
 
-      // Propagate the error
-      throw error;
-    });
+    // Log the error to the console
+    console.error(`Error fetching cat for breed ${breedId}:`, error);
+
+    // Propagate the error
+    throw error;
+  }
 };
